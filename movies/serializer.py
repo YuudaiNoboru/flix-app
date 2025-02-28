d2b2ca10383b4aa9a43c206f0626f39edccee66b
Lucas <vasconcelos.contabeis@gmail.com>
2025-02-28 13:13:39 -0300

    Alteração das validações do serializer de Movie.

--- movies/serializer.py
+++ movies/serializer.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
@@ -10,16 +11,19 @@ class MovieSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_rate(self, obj):
-        raise NotImplementedError('Falta implementar a função.')
+        rate = obj.reviews.aggregate(rate=Avg('stars'))['stars__avg']
+        if rate:
+            return round(rate, 1)
+        return None
 
     def validate_release_data(self, value):
-        if value.year < 1990:
+        if value.year < 1900:
             raise serializers.ValidationError('A data de lançamento não pode \
-                                              ser anterior a 1990.')
+                                              ser anterior a 1900S.')
         return value
 
     def validate_resume(self, value):
-        if len(value) > 200:
+        if len(value) > 500:
             raise serializers.ValidationError('Resumo não deve ser maior que \
-                                              200 caracteres.')
+                                              500 caracteres.')
         return value
